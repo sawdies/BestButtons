f1f8c7827ae8a747fe7db5ccea1f4031613d0c9a
sawdies <dmtrjpv.sds@gmail.com>
2024-03-13 10:39:24 +0700

    Добавьте файлы проекта.

---
+++ BestButtons/Button.cs
@@ -0,0 +1,69 @@
+﻿namespace BestButtons
+{
+    public class Button
+    {
+        public string _name;
+        public bool _activity;
+        public ConsoleColor _color;
+        public Action buttonAction;
+        public Button(string name, Action action, ConsoleColor color = ConsoleColor.Gray)
+        {
+            _name = name;
+            _activity = false;
+            _color = color;
+            buttonAction = action;
+        }
+        public static void PrintButtons(Button[] array)
+        {
+            foreach (Button b in array)
+            {
+                if (b._activity)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = b._color;
+                    Console.WriteLine($"  {b._name}  ");
+                }
+                else
+                {
+                    Console.ForegroundColor = b._color;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"  {b._name}  ");
+                }
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+        protected static void PrintSingleButton(Button[] array, int index)
+        {
+            if (array[index]._activity)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = array[index]._color;
+                Console.WriteLine($"  {array[index]._name}  ");
+            }
+            else
+            {
+                Console.ForegroundColor = array[index]._color;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine($"  {array[index]._name}  ");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        public static void SwitchButton(Button[] array, int activeButtonIndex, int nextActiveButton)
+        {
+            foreach (Button b in array)
+            {
+                b._activity = false;
+            }
+            array[activeButtonIndex]._activity = false;
+            array[nextActiveButton]._activity = true;
+            Console.SetCursorPosition(0, activeButtonIndex);
+            PrintSingleButton(array, activeButtonIndex);
+            Console.SetCursorPosition(0, nextActiveButton);
+            PrintSingleButton(array, nextActiveButton);
+        }
+        public static void Ignore(){}//действие для пустых кнопок
+    }
+
+}
---
+++ BestButtons/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace BestButtons
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.CursorVisible = false;
+            Menu();
+        }
+        static void Menu()
+        {
+            Button[] buttons = [
+                new Button("1", PrintHello),
+                new Button("2", PrintBye),
+            ];
+            int buttonsCount = buttons.Length;
+            int activeButtonIndex = 0;
+            int nextButtonIndex = 0;
+            buttons[0]._activity = true;
+            Console.Clear();
+            Button.PrintButtons(buttons);
+            while (true) {
+                switch (Console.ReadKey(true).Key) {
+                    case ConsoleKey.Escape: Environment.Exit(0); break;
+                    case ConsoleKey.Enter: buttons[activeButtonIndex].buttonAction(); break;
+                    case ConsoleKey.W: case ConsoleKey.LeftArrow: case ConsoleKey.NumPad4:
+                    case ConsoleKey.A: case ConsoleKey.UpArrow: case ConsoleKey.NumPad8:
+                        nextButtonIndex = activeButtonIndex--; break;
+                    case ConsoleKey.S: case ConsoleKey.RightArrow: case ConsoleKey.NumPad6:
+                    case ConsoleKey.D: case ConsoleKey.DownArrow: case ConsoleKey.NumPad2:
+                        nextButtonIndex = activeButtonIndex++; break;
+                }
+                if (activeButtonIndex < 0) activeButtonIndex = buttonsCount - 1;
+                else if (activeButtonIndex >= buttonsCount) activeButtonIndex = 0;
+                Button.SwitchButton(buttons, nextButtonIndex, activeButtonIndex);
+            }
+        }
+        public static void PrintHello() => Console.Write("Hello");
+        public static void PrintBye() => Console.Write("Bye");
+    }
+}
